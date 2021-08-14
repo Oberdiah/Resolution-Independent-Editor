6b9c88ad39639739d518bb263dc51a7de8fcd6f6
Oberdiah <oberdead@gmail.com>
2021-08-14 15:18:12 +0100

    Updated rendering to flip result

--- js/bakeToDisk.js
+++ js/bakeToDisk.js
@@ -7,17 +7,23 @@ function bakeToDisk() {
     uniformInfo.scale = 1/scale;
     uniformInfo.resolution = [renderWidth, renderHeight];
     uniformInfo.buffStartPos = 0;
-    let outputArray = new Uint8ClampedArray(renderWidth * renderHeight * 4);
+    let bkg = createFramebuffer(renderWidth, renderHeight);
+    renderCheckerboard(bkg);
     renderWithBackground(
-        createFramebuffer(renderWidth, renderHeight),
+        bkg,
         createFramebuffer(renderWidth, renderHeight)
     );
+    let outputArray = new Uint8ClampedArray(renderWidth * renderHeight * 4);
     gl.readPixels(0, 0, renderWidth, renderHeight, gl.RGBA, gl.UNSIGNED_BYTE, outputArray);
     let canvas = document.createElement("canvas");
     canvas.width = renderWidth;
     canvas.height = renderHeight;
-    let img = new ImageData(outputArray, renderWidth, renderHeight);
-    canvas.getContext("2d").putImageData(img, 1, 1);
+    let ctx = canvas.getContext("2d");
+    ctx.putImageData(new ImageData(outputArray, renderWidth, renderHeight), 0, 0);
+    ctx.translate(0, renderHeight);
+    ctx.scale(1, -1);
+    ctx.globalCompositeOperation = 'copy';
+    ctx.drawImage(canvas, 0, 0);
     let url = canvas.toDataURL();
     downloadURL(url, "Render.png");
 }
