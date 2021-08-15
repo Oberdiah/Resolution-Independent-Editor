d98967bd36a78bc0bdf1e36e8f48d1890d1e2c86
Oberdiah <oberdead@gmail.com>
2021-08-15 14:38:30 +0100

    Small fixes

--- js/bakeToDisk.js
+++ js/bakeToDisk.js
@@ -13,17 +13,24 @@ function bakeToDisk() {
         createFramebuffer(renderWidth, renderHeight)
     );
 
-    let outputArray = new Uint8ClampedArray(renderWidth * renderHeight * 4);
-    gl.readPixels(0, 0, renderWidth, renderHeight, gl.RGBA, gl.UNSIGNED_BYTE, outputArray);
-    let canvas = document.createElement("canvas");
-    canvas.width = renderWidth;
-    canvas.height = renderHeight;
-    let ctx = canvas.getContext("2d");
-    ctx.putImageData(new ImageData(outputArray, renderWidth, renderHeight), 0, 0);
-    ctx.translate(0, renderHeight);
-    ctx.scale(1, -1);
-    ctx.globalCompositeOperation = 'copy';
-    ctx.drawImage(canvas, 0, 0);
-    let url = canvas.toDataURL();
-    downloadURL(url, "Render.png");
+    try {
+        let outputArray = new Uint8ClampedArray(renderWidth * renderHeight * 4);
+        gl.readPixels(0, 0, renderWidth, renderHeight, gl.RGBA, gl.UNSIGNED_BYTE, outputArray);
+        let canvas = document.createElement("canvas");
+        canvas.width = renderWidth;
+        canvas.height = renderHeight;
+        let ctx = canvas.getContext("2d");
+        ctx.putImageData(new ImageData(outputArray, renderWidth, renderHeight), 0, 0);
+        ctx.translate(0, renderHeight);
+        ctx.scale(1, -1);
+        ctx.globalCompositeOperation = 'copy';
+        ctx.drawImage(canvas, 0, 0);
+        let url = canvas.toDataURL();
+        downloadURL(url, "Render.png");
+    } catch(err) {
+        alert("Your browser is unable to render an image that large " +
+            "(" + renderWidth + "x" + renderHeight + "). " +
+            "Try a smaller render scale or a different browser.");
+    }
+
 }
