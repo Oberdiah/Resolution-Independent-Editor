01a1bcd9152f1ce0f3a919bc43014e6318b9a474
Oberdiah <oberdead@gmail.com>
2021-08-15 17:25:32 +0100

    Attempt to solve issue yet again

--- js/shaderLoader.js
+++ js/shaderLoader.js
@@ -2,21 +2,39 @@ let mainProgram;
 let overlayProgram;
 let checkerboardProgram;
 
-allPromises = [
-    fetch('../shaders/fragMain.mjs').then(response => response.text()),
-    fetch('../shaders/fragOverlay.mjs').then(response => response.text()),
-    fetch('../shaders/fragCheckerboard.mjs').then(response => response.text()),
-    fetch('../shaders/vertMain.mjs').then(response => response.text()),
-]
+let localFetch = '../shaders/'
+let githubFetch = 'https://raw.githubusercontent.com/Oberdiah/Resolution-Independent-Editor/master/shaders/'
 
-Promise.all(allPromises).then((values) => {
+grabShaders(localFetch)
+
+function attemptToLoad(values, preString) {
     window.fragMain = values[0];
     window.fragOverlay = values[1];
     window.fragCheckerboard = values[2];
     window.vertMain = values[3];
-    mainProgram = twgl.createProgramInfo(gl, [vertMain, fragMain])
-    overlayProgram = twgl.createProgramInfo(gl, [vertMain, fragOverlay])
-    checkerboardProgram = twgl.createProgramInfo(gl, [vertMain, fragCheckerboard])
-    loadDefines();
-    requestAnimationFrame(render);
-})
+    if (values[0] !== '') {
+        mainProgram = twgl.createProgramInfo(gl, [vertMain, fragMain])
+        overlayProgram = twgl.createProgramInfo(gl, [vertMain, fragOverlay])
+        checkerboardProgram = twgl.createProgramInfo(gl, [vertMain, fragCheckerboard])
+        loadDefines();
+        requestAnimationFrame(render);
+    } else {
+        if (preString === localFetch) {
+            grabShaders(githubFetch);
+        }
+    }
+}
+
+function grabShaders(preString) {
+    console.log("Trying " + preString);
+    let allPromises = [
+        fetch(preString + 'fragMain.mjs').then(response => response.text()),
+        fetch(preString + 'fragOverlay.mjs').then(response => response.text()),
+        fetch(preString + 'fragCheckerboard.mjs').then(response => response.text()),
+        fetch(preString + 'vertMain.mjs').then(response => response.text()),
+    ]
+    Promise.all(allPromises).then((values) => {
+        attemptToLoad(values, preString)
+    })
+}
+
